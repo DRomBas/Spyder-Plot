93ec94ebd88f7404031f72119d82de203bc60619
David RomBas <50577357+DRomBas@users.noreply.github.com>
2020-04-24 09:45:21 +0200

    First version

---
+++ example_spyderPlot.m
@@ -0,0 +1,16 @@
+close all;clc;clearvars;
+
+N = 160;
+M = 20;
+Data = 18+1*randn(1,M) + 2.*randn(N,M);
+
+% Example 1
+subplot(121);
+spyderPlot(Data)
+
+% Example 2
+subplot(122);
+param.plotDots = false;
+param.superior = 90;
+param.inferior = 10;
+spyderPlot(Data,param)
